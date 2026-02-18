89d8e8dcdeab1d8df302e0cf44eaec47120e2c5e
Baraa Jazah <129511800+BaraaJazah@users.noreply.github.com>
2026-02-18 23:52:28 +0100

    Delete reMyDailyCalories/hooks directory
    delete hooks

--- reMyDailyCalories/hooks/calcBurnForExercise.ts
+++
@@ -1,9 +0,0 @@
-
-const calcBurnForExercise = (weight, minute, met) => {
-    const burn = weight * (minute / 60) * met
-    return burn.toFixed(0) 
-
-}
-
-
-export { calcBurnForExercise, }
--- reMyDailyCalories/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import { calcNeedKcal } from "./calcNeedKcal";
-import { jsonToStrForFoodIA } from "./jsonToStrForFoodIA";
-
-
-export { calcNeedKcal,
-    jsonToStrForFoodIA
- }
