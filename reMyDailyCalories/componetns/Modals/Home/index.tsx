f159ae07ad3c99ae8932111fc631aa46c93f2dbd
Baraa Jazah <129511800+BaraaJazah@users.noreply.github.com>
2026-02-19 05:41:54 +0100

    Delete reMyDailyCalories/componetns/Modals/Home directory

--- reMyDailyCalories/componetns/Modals/Home/index.tsx
+++
@@ -1,5 +0,0 @@
-import ModalHeaderList from "./ModalHeaderList";
-import ModalCalender from "./ModalCalender";
-import ModalUpdate from "./ModalUpdate";
-
-export { ModalHeaderList, ModalCalender, ModalUpdate };
